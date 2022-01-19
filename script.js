4f02497d324f9832a2b15d6f315d69c1df1b2ec1
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-01-19 11:00:51 -0500

    End of lecture on week 1

---
+++ script.js
@@ -0,0 +1,39 @@
+// $(document).ready(function () {
+$(function () {
+
+    console.log("Got here");
+
+    let $hotLi = $('li.hot');
+    $hotLi.hide().fadeIn(2000);
+
+    // $('li.hot').hide().fadeIn(2000);
+
+    let $title = $('.title');
+    $title.hide();
+
+    let $author = $('#author');
+    $author.css("background-color", "red");
+    $author.css({
+        'font-family': 'Arial',
+        'color': "#999999"
+    });
+
+    let $first = $('ul#list li:first-child');
+    $first.css("background-color", "green");
+
+    console.log("list item html", $('li').html());
+    console.log("list item text", $('li').text());
+
+    $('li').html("<strong>Updated!</strong>");
+    // $('li').text("All the same");
+
+    // $('li').each(function () {
+    //     let ids = this.id;
+    //     $(this).append(" " + ids);
+    // });
+
+    $('li').on('click', function (e) {
+        console.log(this.id, "event object is: ", e);
+    });
+
+});
