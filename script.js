4bdf1c957b2a7038a537c333ab27551f7021d48b
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-01-25 14:39:50 -0500

    Completed class examples for week 02

--- script.js
+++ script.js
@@ -1,10 +1,61 @@
-let xhr = new XMLHttpRequest;
+$(function () {
+    let xhr = new XMLHttpRequest;
 
-xhr.open('GET', 'test.json', true);
+    // request for the html file
+    // xhr.open('GET', 'test.html', true);
 
+    // request for the xml file
+    // xhr.open('GET', 'test.xml', true);
 
-xhr.send("search=arduino");
+    // request for the json file
+    xhr.open('GET', 'test.json', true);
 
+    xhr.onload = function () {
+        if (xhr.status === 200) // successfully retrieved content
+        {
+            // process the data if it's html
+            // let el = document.getElementById("ajax-data-wrapper");
+            // el.innerHTML = xhr.responseText; // throw the html directly on the page
+
+            // process data if it's xml
+            // let el = document.getElementById("ajax-data-wrapper");
+            // let xmlData = xhr.responseXML;  // we need to store the data somewhere so we can convert it to html
+            // // console.log(xmlData.getElementsByTagName('city')[0]);
+
+            // let city = document.createElement("div");
+            // let name = document.createElement("div");
+            // let nameText = document.createTextNode(xmlData.getElementsByTagName('city')[0].children[0].textContent)
+            // let population = document.createElement("div");
+            // let populationText = document.createTextNode(xmlData.getElementsByTagName('city')[0].children[1].textContent)
+
+            // name.appendChild(nameText);
+            // population.appendChild(populationText);
+            // city.appendChild(name);
+            // city.appendChild(population);
+            // el.append(city);
+
+            // process data if it's json
+            let jsonData = xhr.responseText; // get the json value that came back from the server
+            let jsonObject = JSON.parse(jsonData);
+            console.log("The raw json data is: ", jsonData);
+            console.log("The json object data is: ", jsonObject);
+            $('#ajax-data-wrapper').html("<div class='city'><div class='name'>" +
+                jsonObject.cities[0].name +
+                "</div><div class='citymap'><img src='" +
+                jsonObject.cities[0]["imageURL"] + "'></div></div>");
+
+            // process the json data somehow
+
+            let newJsonDataAsAString = JSON.stringify(jsonObject);
+            console.log("The new json stringified data looks like: ", newJsonDataAsAString);
+        }
+        else {
+            console.log("something has gone terribly wrong");
+        }
+    };
+    xhr.send("search=arduino");
+
+});
 
 
 // // $(document).ready(function () {
