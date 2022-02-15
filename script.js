fc847e4f5ba13612c81c969d7d5030829cbf3dda
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-02-15 14:34:16 -0500

    End of class on week 05

--- script.js
+++ script.js
@@ -1,43 +1,32 @@
-// $(function () {
+$(function () {
+    // navigator.geolocation.getCurrentPosition(success, () => {
+    //     console.log("Please enable geolocation in order to see your coordinates");
+    // });
 
-// });
-var a;
-var b;
-var c = { property: "Something", numbervalue: 1 };
+    // function success(position) {
 
-function step1() {
-    // console.log("step 1 ran");
-    return "this" + step2();
-}
-function step2() {
-    a = [1, 2, 3, 4];
-    // console.assert(a.length > 0, "array a STILL has no value");
-    b = a[100];
-    // debugger;
-    // ILLEGAL
-    // console.log("got here", b);
-    return " is less awesome";
-}
+    //     console.log("Position object: ", position);
+    //     console.log("latitude: ", position.coords.latitude, ", longitude: ", position.coords.longitude);
+    //     $("div").html(position.timestamp);
 
-try {
-    console.log("trying to run some code");
-    var message = step1();
-    console.log("successfully ran those functions");
-}
-catch (exception) {
-    console.error("something went wrong, info as follows: ", exception);
-}
-finally {
-    console.log("This is going to run no matter what");
-}
-// console.log("a is: ", a, " and b is: ", b, " and c is ", c, " and c if I force it to be a string is: " + c);
-console.group("examples of messages")
-console.info(message);
-console.log(message);
-console.warn(message);
-console.error(message);
-console.groupEnd();
+    // }
 
-// console.table(c);
+    // localStorage.setItem("food", "apple");
+    let foodItem = localStorage.getItem("food");
+    console.log(foodItem);
 
-// console.log("the console error didn't halt the code");
+    let apple = {
+        type: "fruit",
+        name: "apple",
+        taste: "sweet"
+    };
+    localStorage.setItem("foodObject", JSON.stringify(apple));
+    let foodJSONStringObject = localStorage.getItem("foodObject");
+    let foodObject = JSON.parse(foodJSONStringObject);
+    console.log("my food object: ", foodObject.name);
+
+    $('#prizes').accordion();
+
+});
+
+
