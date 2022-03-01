20d0e12ee2bba542997bf396c8f8ab0ff4a398ce
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-03-01 14:17:54 -0500

    end of class on week 07 for objects and classes

--- script.js
+++ script.js
@@ -1,32 +1,104 @@
-$(function () {
-    // navigator.geolocation.getCurrentPosition(success, () => {
-    //     console.log("Please enable geolocation in order to see your coordinates");
-    // });
+// the first object example
 
-    // function success(position) {
-
-    //     console.log("Position object: ", position);
-    //     console.log("latitude: ", position.coords.latitude, ", longitude: ", position.coords.longitude);
-    //     $("div").html(position.timestamp);
-
-    // }
-
-    // localStorage.setItem("food", "apple");
-    let foodItem = localStorage.getItem("food");
-    console.log(foodItem);
-
-    let apple = {
-        type: "fruit",
-        name: "apple",
-        taste: "sweet"
-    };
-    localStorage.setItem("foodObject", JSON.stringify(apple));
-    let foodJSONStringObject = localStorage.getItem("foodObject");
-    let foodObject = JSON.parse(foodJSONStringObject);
-    console.log("my food object: ", foodObject.name);
-
-    $('#prizes').accordion();
-
-});
+// window.color = "blue";
 
 
+// let whiteRabbit = {};
+// whiteRabbit.color = "white";
+// whiteRabbit.name = "Nervous guy";
+// // whiteRabbit.changeColor = () => this.color = "red";
+// whiteRabbit.changeColor = function () {
+//     return this.color = "red";
+// }
+// whiteRabbit.speak = function (line) {
+//     console.log(`The ${this.color} rabbit says "${line}. His name is ${this.name}"`);
+//     this.changeColor();
+// };
+// console.log("The global color (attempt 1) is : ", window.color);
+// whiteRabbit.speak("I'm late!");
+// console.log("The global color (attempt 2) is : ", window.color);
+// whiteRabbit.speak("I'm late!");
+// console.log("The global color (attempt 3) is : ", window.color);
+
+// the second object example
+
+// let a = [1, 2, 3, 4, 5];
+// let b = { color: "white" };
+// let c = new Object();
+// c.color = "blue";
+
+// console.log("Array toString invoked " + a);
+// console.log("Object toString invoked ", b);
+
+// console.log(Object.getPrototypeOf(a) === Array.prototype);
+// console.log(Object.getPrototypeOf(Array.prototype) === Object.prototype);
+// console.log(Object.getPrototypeOf(Object.prototype));
+
+// the third object example
+
+// function Animal(type) {
+//     this.animalType = type;
+// }
+// // Animal.animalType = "lion";
+// Animal.prototype.toString = function () {
+//     return "This animal is a " + this.animalType;
+// }
+// let fluffyRabbit = new Animal("Rabbit");
+// console.log(fluffyRabbit + "");
+// let feriociousFrog = new Animal("Frog");
+// console.log(feriociousFrog + "");
+
+// the fourth object example
+// class Animal {
+//     // animalType; // property belonging to the class
+//     constructor(type) {
+//         this.animalType = type;
+//         this.color = "blue";
+//     }
+//     toString() {
+//         return "This animal is a " + this.animalType;
+//     }
+// }
+
+// let rabbit = new Animal("Rabbit");
+// let rabbitString = `The animal says: ${rabbit}`;
+// // note: console.log doesn't invoke toString
+// console.log(rabbit);
+// console.log(rabbitString);
+
+// the fifth object example
+
+class Temperature {
+    // celsius;
+    constructor(celsius) {
+        this.celsius = celsius;
+    }
+    get fahrenheit() {
+        return this.celsius * 1.8 + 32;
+    }
+    set fahrenheit(newTemp) {
+        this.celsius = parseFloat(((newTemp - 32) / 1.8).toFixed(2));
+    }
+    somestring() {
+        return "something";
+    }
+}
+let temper = new Temperature(12);
+console.log(temper.fahrenheit);
+temper.fahrenheit = 80;
+console.log(temper.celsius);
+
+console.log('celsius' in Temperature);
+
+// the sixth object example
+
+const classroom = {
+    size: 20,
+    year: 2,
+    room: '248'
+};
+console.log(classroom.size, " ", classroom["size"]);
+for (const property in classroom) {
+    console.log(`Property name: ${property}`);
+    console.log(`Property value: ${classroom[property]}`);
+}
