10b81e806b74946c0d5091ab08728815689e26de
amillerman <90463478+amillerman01@users.noreply.github.com>
2022-03-23 08:51:52 -0400

    End of lecture on week 09
    Finished modal and photo viewer is working, just missing photos

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function () {
+
+    //call the tabbed, or the accordion, or the modal
+    (function (someParameter) {
+        console.log(someParameter);
+        // Remove modal content from page and store in $content
+        var $content = $('#share-options').detach();
+        var modal = new Modal();
+        // Click handler calls open() method of modal object
+        $('#share').on('click', function () {
+            modal.open({
+                content: $content,
+                width: 340,
+                height: 300
+            });
+        });
+    })("ValuePassedIn");
+    
+});
